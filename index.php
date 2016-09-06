c43b22d2be24060611618af1db4aace4132928db
dmic2010 <dave@dmic.ie>
2016-09-06 18:59:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  *
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * Line added to test how sync works
  */
 
 defined('_JEXEC') or die;
